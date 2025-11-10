579bf7a89ffe64a404bbb433bfbbf1e606a3f916
nikic <nikic.luka2001@gmail.com>
2025-11-10 20:03:12 +0100

    geting guesses from comand

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+    int numOfGuesses = 0;
+    GameHelper helper = new GameHelper();
+    SimpleStartup s = new SimpleStartup();
+    int randomNum = (int) (Math.random() * 5);
+    int[] locationsS = {randomNum, randomNum + 1, randomNum + 2};
+    s.setLocationCells(locationsS);
+    boolean isAlive = true;
+
+    while (isAlive) {
+        int guess = helper.getUserInput("enter a number");
+        String result = s.checkYourself(guess);
+        numOfGuesses++;
+        if (result.equals("kill")) {
+            isAlive = false;
+            System.out.println("You took " + numOfGuesses + " guesses");
+        }
+    }
 
      SimpleStartup dot = new SimpleStartup();
      int[] locations = {2, 3, 4};
