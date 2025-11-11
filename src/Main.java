5bae6943bb96e09f1baf9c27a7ca9789ac7465a4
nikic <nikic.luka2001@gmail.com>
2025-11-11 22:00:01 +0100

    game with arrya list

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-    int numOfGuesses = 0;
-    GameHelper helper = new GameHelper();
-    SimpleStartup s = new SimpleStartup();
-    int randomNum = (int) (Math.random() * 5);
-    int[] locationsS = {randomNum, randomNum + 1, randomNum + 2};
-    s.setLocationCells(locationsS);
-    boolean isAlive = true;
 
-    while (isAlive) {
-        int guess = helper.getUserInput("enter a number");
-        String result = s.checkYourself(guess);
-        numOfGuesses++;
-        if (result.equals("kill")) {
-            isAlive = false;
-            System.out.println("You took " + numOfGuesses + " guesses");
-        }
-    }
 
-     SimpleStartup dot = new SimpleStartup();
-     int[] locations = {2, 3, 4};
-     dot.setLocationCells(locations);
-     int userGuess = 2;
-     String result = dot.checkYourself(userGuess);
-     String testResult = "failed";
-     if(result.equals("hit")) {
-         testResult = "passed";
-     }
-     System.out.println(testResult);
+
+
+
+//    int numOfGuesses = 0;
+//    GameHelper helper = new GameHelper();
+//    SimpleStartup s = new SimpleStartup();
+//    int randomNum = (int) (Math.random() * 5);
+//    int[] locationsS = {randomNum, randomNum + 1, randomNum + 2};
+//    s.setLocationCells(locationsS);
+//    boolean isAlive = true;
+//
+//    while (isAlive) {
+//        int guess = helper.getUserInput("enter a number");
+//        String result = s.checkYourself(guess);
+//        numOfGuesses++;
+//        if (result.equals("kill")) {
+//            isAlive = false;
+//            System.out.println("You took " + numOfGuesses + " guesses");
+//        }
+//    }
+//
+//     SimpleStartup dot = new SimpleStartup();
+//     int[] locations = {2, 3, 4};
+//     System.out.println(locations[0]);
+//     dot.setLocationCells(locations);
+//     int userGuess = 2;
+//     String result = dot.checkYourself(userGuess);
+//     String testResult = "failed";
+//     if(result.equals("hit")) {
+//         testResult = "passed";
+//     }
+//     System.out.println(testResult);
     }
 }
---
+++ src/StartupBust.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class StartupBust {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<Startup> startups = new ArrayList<>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame() {
+        Startup one = new Startup();
+        one.setName("poniez");
+        Startup two = new Startup();
+        two.setName("hacqi");
+        Startup three = new Startup();
+        three.setName("cabista");
+        startups.add(one);
+        startups.add(two);
+        startups.add(three);
+
+        System.out.println("Your goal is to sink three Startups.");
+        System.out.println("poniez, hacqi, cabista");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        for (Startup startup : startups) {
+            ArrayList<String> newLocation = helper.placeStartup(3);
+            startup.setLocationCells(newLocation);
+        }
+    }
+
+    private void startPlaying() {
+        while(!startups.isEmpty()) {
+            String userGuess = helper.getUserInput("Enter a guess ");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+}
