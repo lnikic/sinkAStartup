036004cec29e27ae3658d9030373863d81ad6692
nikic <nikic.luka2001@gmail.com>
2025-11-12 20:12:07 +0100

    finsih startup bust game

--- src/Startup.java
+++ src/Startup.java
@@ -3,9 +3,14 @@ import java.util.ArrayList;
 public class Startup {
 
     private ArrayList<String> locationCells;
+    private String name;
 
     public void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
     }
 
     public String checkYourself(String userInput) {
@@ -15,6 +20,7 @@ public class Startup {
             locationCells.remove(index);
             if (locationCells.isEmpty()) {
                 result = "kill";
+                System.out.println("Ouch! You sunk " + name + "   : ( ");
             } else {
                 result = "hit";
             }
--- src/StartupBust.java
+++ src/StartupBust.java
@@ -5,7 +5,7 @@ public class StartupBust {
     private ArrayList<Startup> startups = new ArrayList<>();
     private int numOfGuesses = 0;
 
-    private void setUpGame() {
+    public void setUpGame() {
         Startup one = new Startup();
         one.setName("poniez");
         Startup two = new Startup();
@@ -26,11 +26,40 @@ public class StartupBust {
         }
     }
 
-    private void startPlaying() {
-        while(!startups.isEmpty()) {
+    public void startPlaying() {
+        while (!startups.isEmpty()) {
             String userGuess = helper.getUserInput("Enter a guess ");
             checkUserGuess(userGuess);
         }
         finishGame();
+    }
+
+    public void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "miss";
+
+        for (Startup startupToTest : startups) {
+            result = startupToTest.checkYourself(userGuess);
+
+            if (result.equals("hit")) {
+                break;
+            }
+            if (result.equals("kill")) {
+                startups.remove(startupToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    public void finishGame() {
+        System.out.println("All Startups are dead! Your stock is now worthless");
+        if (numOfGuesses <= 18) {
+            System.out.println("It only took you " + numOfGuesses + " guesses.");
+            System.out.println("You got out before your options sank.");
+        } else {
+            System.out.println("Took you long enough. " + numOfGuesses + " guesses.");
+            System.out.println("Fish are dancing with your options");
+        }
     }
 }
